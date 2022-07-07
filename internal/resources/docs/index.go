86609287440cc0d8da83ddf5ffc02aa08513c640
dh <dh>
2022-07-07 18:06:25 +0800

    init

--- internal/resources/docs/index.go
+++ internal/resources/docs/index.go
@@ -9,28 +9,14 @@ import (
 	"github.com/kataras/iris/v12/cache"
 )
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:8080
-// @BasePath /v2
 func RegisterRouter(app *iris.Application) {
 	swaggerUI := swagger.Handler(swaggerFiles.Handler,
 		swagger.URL("/assets/doc.yaml"), // The url pointing to API definition.
 		swagger.DeepLinking(true),
-		swagger.Prefix("/swagger"),
+		swagger.Prefix("/apidoc"),
 	)
 	// 设置资源缓存，缓存10秒，之后会被清除并重置
-	cacheHandler := cache.Handler(10 * time.Second)
-	app.Get("/swagger", cacheHandler, swaggerUI)
-	app.Get("/swagger/{any:path}", swaggerUI)
+	cacheHandler := cache.Handler(1 * time.Second)
+	app.Get("/apidoc", cacheHandler, swaggerUI)
+	app.Get("/apidoc/{any:path}", swaggerUI)
 }
